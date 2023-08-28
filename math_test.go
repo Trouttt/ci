e6920609b67dbd0a1d1d644e9ad4dbc3b7a8fed4
leonardo <desu987@hotmail.com>
2023-08-28 20:54:58 -0300

    test: teste errado

--- math_test.go
+++ math_test.go
@@ -4,7 +4,7 @@ import "testing"
 func SomaTest(t *testing.T) {
 	totalSum := Soma(10, 10)
 
-	if totalSum != 20 {
+	if totalSum != 25 {
 		t.Errorf("Resultado %d. Esperado: %d", totalSum, 20)
 	}
 }
