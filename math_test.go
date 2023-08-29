6eda4cef8e1a4ee8a071f232b4c033657b778ef6
leonardo <desu987@hotmail.com>
2023-08-28 22:25:42 -0300

    test: mudado valor pra 30

--- math_test.go
+++ math_test.go
@@ -2,9 +2,9 @@ package main
 import "testing"
 
 func TestSoma(t *testing.T) {
-	totalSum := Soma(10, 10)
+	totalSum := Soma(15, 15)
 
-	if totalSum != 25 {
-		t.Errorf("Resultado %d. Esperado: %d", totalSum, 25)
+	if totalSum != 30 {
+		t.Errorf("Resultado %d. Esperado: %d", totalSum, 30)
 	}
 }
