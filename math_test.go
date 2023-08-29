dea5e63b24d828b0b12bf7e0415ae95a19f270c6
leonardo <desu987@hotmail.com>
2023-08-28 22:22:46 -0300

    test: mudado valor pra 25

--- math_test.go
+++ math_test.go
@@ -5,6 +5,6 @@ func TestSoma(t *testing.T) {
 	totalSum := Soma(10, 10)
 
 	if totalSum != 25 {
-		t.Errorf("Resultado %d. Esperado: %d", totalSum, 20)
+		t.Errorf("Resultado %d. Esperado: %d", totalSum, 25)
 	}
 }
